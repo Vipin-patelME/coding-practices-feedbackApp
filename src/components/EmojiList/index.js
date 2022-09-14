5ac6e8e88247cf4f85eb45602a5a64e4e5276895
Vipin Patel <112804686+Vipin-patelME@users.noreply.github.com>
2022-09-14 17:06:03 +0530

    Update index.js

--- src/components/EmojiList/index.js
+++ src/components/EmojiList/index.js
@@ -4,10 +4,10 @@ const EmojiList = props => {
   const {eachEmoji} = props
   const {name, imageUrl} = eachEmoji
   return (
-    <div className="emoji-item">
+    <li className="emoji-item">
       <img src={imageUrl} alt="Emoji" className="emoji-style" />
       <p className="paragraph">{name}</p>
-    </div>
+    </li>
   )
 }
 export default EmojiList
